fe0b82ff995644e4ccfadc033158d8cbe207cebf
ParkJun <bruno.park@adwitt.com>
2024-05-31 17:32:47 +0900

    fix: 이름 수정

--- Package.swift
+++ Package.swift
@@ -4,26 +4,26 @@
 import PackageDescription
 
 let package = Package(
-  name: "NestAdsSdk",
+  name: "NestAdsSDK",
   platforms: [.iOS(.v12)],
   products: [
     .library(
-      name: "NestAdsSdk",
-      targets: ["NestAdsSdkWrapper"]
+      name: "NestAdsSDK",
+      targets: ["NestAdsSDKWrapper"]
     ),
   ],
   dependencies: [
     .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.1"),
-    .package(url: "https://github.com/realm/realm-swift.git", from: "10.36.0"),
+    .package(url: "https://github.com/realm/realm-swift.git", from: "10.49.3"),
   ],
   targets: [
-    .target(name: "NestAdsSdkWrapper",
+    .target(name: "NestAdsSDKWrapper",
             dependencies: [
-              .target(name: "NestAdsSdk", condition: .when(platforms: [.iOS])),
+              .target(name: "NestAdsSDK", condition: .when(platforms: [.iOS])),
               .product(name: "RealmSwift", package: "realm-swift"),
               .product(name: "CryptoSwift", package: "CryptoSwift"),
 
             ]),
-    .binaryTarget(name: "NestAdsSdk", path: "Sources/NestAdsSdk.xcframework"),
+    .binaryTarget(name: "NestAdsSDK", path: "Sources/NestAdsSDK.xcframework"),
   ]
 )
